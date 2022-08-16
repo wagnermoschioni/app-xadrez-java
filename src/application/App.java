a8160bed499722ac10c43d1f6da2a65db7664422
wagnermosch <jose.w.moschioni@accenture.com>
2022-08-16 00:40:59 -0300

    Classes Board e Piece

--- src/application/App.java
+++ src/application/App.java
@@ -1,13 +1,12 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class App {
   public static void main(String[] args) {
-	System.out.println("Hello");
 	
-	Position pos = new Position(3, 6);
-	
-	System.out.println(pos);
+	Board board = new Board(8, 8);
+	System.out.println(board);
 }
 }
---
+++ src/boardgame/Board.java
@@ -0,0 +1,35 @@
+package boardgame;
+
+public class Board {
+
+	private int rows;
+	private int collumns;
+	
+	private Piece[][] pieces;
+
+	public Board(int rows, int collumns) {
+		this.collumns = collumns;
+		this.rows= rows;
+		this.pieces = new Piece[rows][collumns];
+	}
+
+	public int getRows() {
+		return rows;
+	}
+
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+
+	public int getCollumns() {
+		return collumns;
+	}
+
+	public void setCollumns(int collumns) {
+		this.collumns = collumns;
+	}
+	
+	
+	
+	
+}
