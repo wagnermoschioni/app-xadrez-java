d0047d9de410129bb3a1699c631c5e9adabfb19b
wagnermosch <jose.w.moschioni@accenture.com>
2022-08-16 00:27:08 -0300

    classe Position

--- src/application/App.java
+++ src/application/App.java
@@ -1,7 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class App {
   public static void main(String[] args) {
 	System.out.println("Hello");
+	
+	Position pos = new Position(3, 6);
+	
+	System.out.println(pos);
 }
 }
